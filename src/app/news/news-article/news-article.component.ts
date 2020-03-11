84d76e790ac87aed168cbeca5b6828dfc4bb7b2f
ahmed <ahmed.michrgui@hotmail.fr>
2020-03-12 00:23:21 +0100

    article social buttons reworked

--- src/app/news/news-article/news-article.component.ts
+++ src/app/news/news-article/news-article.component.ts
@@ -1,18 +1,25 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-news-article',
   templateUrl: './news-article.component.html',
   styleUrls: ['./news-article.component.css']
 })
-export class NewsArticleComponent implements OnInit {
+export class NewsArticleComponent implements OnInit, OnDestroy {
+
   currentURL: string;
 
   constructor() {
   }
 
   ngOnInit() {
-   this.currentURL = window.location.href;
+    this.currentURL = window.location.href;
   }
 
+  ngOnDestroy(): void {
+  }
+
+  onShare() {
+        window.alert('sharing button cannot be dynamic on front-end! \n they will be implemented later with nodejs');
+  }
 }
