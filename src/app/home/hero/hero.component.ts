77a0a737f85581e4d75b09a91a9276ce0c33c7ac
ahmed <ahmed.michrgui@hotmail.fr>
2020-03-29 18:52:49 +0100

    before really going dynamic

--- src/app/home/hero/hero.component.ts
+++ src/app/home/hero/hero.component.ts
@@ -16,14 +16,13 @@ export class HeroComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
+    this.loadScripts();
+
     document.getElementById('header').classList.add('header-transparent');
     document.getElementById('logo').classList.add('hide-on-sd');
     document.getElementById('header').classList.remove('box-shadow');
-
     // tslint:disable-next-line:max-line-length
     document.getElementById('hero').style.setProperty('--bg-url', 'linear-gradient(to right, rgba(30, 67, 86, 0.8), rgba(30, 67, 86, 0.6)), url("assets/img/' + this.imageName + '") center top no-repeat');
-
-    this.loadScripts();
   }
 
   ngOnDestroy(): void {
