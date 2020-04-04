af63ea69a911fb47ea09771a5fe42202db7bc6d9
ahmed <ahmed.michrgui@hotmail.fr>
2020-04-04 17:16:31 +0100

    static order-form done

--- src/app/moovobrain/moovobrain-forms/form-order/form-order.component.ts
+++ src/app/moovobrain/moovobrain-forms/form-order/form-order.component.ts
@@ -12,9 +12,18 @@ export class FormOrderComponent implements OnInit {
   trainingSessionCheckbox = false;
   controlCheckbox = false;
   moreYearsRadio = false;
-
-  controlPrice = 7600;
+  controlMorePrice = 7600;
   clientNature = 0;
+  // this doesn't include the services , it shall not be presented alone to user
+  purchasePrice = 15000;
+  // training price and control price is hold in its own variable and method because radio buttons are causing problems since
+  // they don't consider uncheck as a change event
+  trainingPrice = 0;
+  controlPrice = 0;
+  // when disabling training session or quarterly control , this variable will hold the old price value
+  // in case the user enables it again
+  trainingPriceBeforeDisabling = 0;
+  controlPriceBeforeDisabling = 0;
 
   constructor(private renderer2: Renderer2, private scriptsLoader: ScriptsLoaderService) {
   }
@@ -58,7 +67,7 @@ export class FormOrderComponent implements OnInit {
       iCountry: new FormControl('', [this.countrySelected.bind(this)]),
       iPhone: new FormControl(null, [Validators.required, Validators.min(0), Validators.minLength(8)]),
       iHandicap: new FormControl(null, [Validators.required]),
-    //  Organisation form
+      //  Organisation form
       oName: new FormControl(null, [Validators.required]),
       oType: new FormControl(null, [Validators.required]),
       oSector: new FormControl(null, [Validators.required]),
@@ -80,7 +89,10 @@ export class FormOrderComponent implements OnInit {
 
     if (this.trainingSessionCheckbox) {
       this.form.get('trainingHours').enable();
+      this.trainingPrice = this.trainingPriceBeforeDisabling;
     } else {
+      this.trainingPriceBeforeDisabling = this.trainingPrice;
+      this.trainingPrice = 0;
       this.form.get('trainingHours').disable();
     }
   }
@@ -90,7 +102,10 @@ export class FormOrderComponent implements OnInit {
 
     if (this.controlCheckbox) {
       this.form.get('controlYears').enable();
+      this.controlPrice = this.controlPriceBeforeDisabling;
     } else {
+      this.controlPriceBeforeDisabling = this.controlPrice;
+      this.controlPrice = 0;
       this.form.get('controlYears').disable();
     }
   }
@@ -100,7 +115,8 @@ export class FormOrderComponent implements OnInit {
   }
 
   calculateControlPrice(addedYears: number) {
-    this.controlPrice = 4000 + 3600 * (addedYears - 2);
+    this.controlMorePrice = 4000 + 3600 * (addedYears - 2);
+    this.controlPrice = this.controlMorePrice;
   }
 
   changeClientNature(clientNature: number) {
@@ -113,5 +129,18 @@ export class FormOrderComponent implements OnInit {
 
   alert() {
     alert('function not implemented yet , waiting for nodejs integration');
+  }
+
+  priceUpdate(event: Event, price: number) {
+    const checked = event.target.checked;
+    this.purchasePrice += checked ? price : -price;
+  }
+
+  trainingRadioSelected(event: Event, price: number) {
+    this.trainingPrice = price;
+  }
+
+  controlRadioSelected(event: Event, price: number) {
+    this.controlPrice = price;
   }
 }
