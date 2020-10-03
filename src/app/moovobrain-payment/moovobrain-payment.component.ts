553ac3b1d79c1a8b70c4d6e1da617e3f180e2546
ahmed michrgui <ahmed.michrgui@hotmail.fr>
2020-10-03 19:52:51 +0100

    hero fixed

--- src/app/moovobrain-payment/moovobrain-payment.component.ts
+++ src/app/moovobrain-payment/moovobrain-payment.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {HostURLService} from '../shared/services/host-url.service';
+import {ActivatedRoute} from '@angular/router';
+import {ToastService} from '../shared/services/tost.service';
 
 @Component({
   selector: 'app-moovobrain-payment',
@@ -7,11 +11,16 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./moovobrain-payment.component.css']
 })
 export class MoovobrainPaymentComponent implements OnInit {
+  url = this.urlService.url;
   isPaid = false;
+  isLoading = false;
   form: FormGroup;
   @ViewChild('slipInputLabel', {static: true}) slipInputLabel;
 
-  constructor() {
+  constructor(private http: HttpClient,
+              private urlService: HostURLService,
+              private activatedRoute: ActivatedRoute,
+              private toast: ToastService) {
   }
 
   ngOnInit() {
@@ -24,7 +33,11 @@ export class MoovobrainPaymentComponent implements OnInit {
     });
   }
 
-  // this function to update the "cv" form field whenever a file is selected
+  checkIfOrderExist() {
+
+  }
+
+  // this function to update the "payment slip" form field whenever a file is selected
   onFileChange(event) {
     if (event.target.files.length > 0) {
       const file = event.target.files[0];
@@ -33,6 +46,22 @@ export class MoovobrainPaymentComponent implements OnInit {
       });
       document.getElementById('slipInputLabel').innerHTML = file.name;
     }
+  }
+
+  sendHttpRequest() {
+    const id = this.activatedRoute.snapshot.params.id;
+    const formData = new FormData();
+    formData.append('id', id);
+    formData.append('payment-slip', this.form.get('transferSlip').value);
+
+    this.isLoading = true;
+    this.http.post(this.url + '/moovobrain-payment', formData).subscribe(() => {
+      this.isPaid = true;
+      this.isLoading = false;
+    }, () => {
+      this.toast.error('Unable to confirm your payment.', 'Error :');
+      this.isLoading = false;
+    });
   }
 
 }
