e446ca9eb5a95e695e0b82e8f0f960875b1a643c
ahmed michrgui <ahmed.michrgui@hotmail.fr>
2020-10-10 20:19:33 +0100

    firebase dumped + moovobrain page metadata fixed

--- src/app/moovobrain/moovobrain.component.ts
+++ src/app/moovobrain/moovobrain.component.ts
@@ -1,4 +1,6 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {Meta} from '@angular/platform-browser';
+import {HostURLService} from '../shared/services/host-url.service';
 
 @Component({
   selector: 'app-moovobrain',
@@ -6,14 +8,21 @@ import {AfterViewInit, Component, OnInit} from '@angular/core';
   styleUrls: ['./moovobrain.component.css']
 })
 export class MoovobrainComponent implements OnInit, AfterViewInit {
-
-  constructor() {
+  constructor(private metadata: Meta, private urlService: HostURLService) {
   }
 
   ngOnInit() {
+    this.updateMetadata();
   }
 
   ngAfterViewInit(): void {
+  }
+
+  updateMetadata(): void {
+    this.metadata.updateTag({content: 'Moovobrain'}, `name='title-for-sharer'`);
+    this.metadata.updateTag({content: this.urlService.url + '/assets/img/wheelchair-1.jpeg'}, `name='image-for-sharer'`);
+    // tslint:disable-next-line:max-line-length
+    this.metadata.updateTag({content: 'MOOVOBRAIN is a steering system of powered wheelchairs for physically disabled people who cannot use their upper members to pilot conventional wheelchairs with joystick.'}, `name='description-for-sharer'`);
   }
 
 }
