92a65efe68c39300b09a10b9f5f1cc17a38c09b2
ahmed michrgui <ahmed.michrgui@hotmail.fr>
2020-11-01 16:50:43 +0100

    showing mobile-app

--- src/app/moovobrain/moovobrain-tablet-app/moovobrain-tablet-app.component.ts
+++ src/app/moovobrain/moovobrain-tablet-app/moovobrain-tablet-app.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {ScriptsLoaderService} from '../../scripts-loader.service';
 
 @Component({
   selector: 'app-moovobrain-tablet-app',
   templateUrl: './moovobrain-tablet-app.component.html',
   styleUrls: ['./moovobrain-tablet-app.component.css']
 })
-export class MoovobrainTabletAppComponent implements OnInit {
+export class MoovobrainTabletAppComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(private scriptsLoader: ScriptsLoaderService) {
+  }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.scriptsLoader.addManyScriptsAsync('/assets/vendor/video/video.min.js').then();
   }
 
 }
